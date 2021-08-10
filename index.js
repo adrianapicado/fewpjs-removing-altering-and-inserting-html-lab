ba3b7f21ad3a505dac97c85296df147eaf5aaa95
adriana <picadoadriana7@gmail.com>
2021-08-10 14:41:41 -0400

    commit tree c975942063d60c687029cd518763c800706dc348

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+var newHeader = document.createElement('h1');
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = "ADRIANA is the champion";
+document.body.appendChild(newHeader);
